266245a01395dd1a63970010009ae88db57fed43
bohdanD <doroshbohdan1@gmail.com>
2017-06-12 13:50:37 +0300

    Add GameViewModel

---
+++ MillionaireGame.Frontend/Models/GameViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MillionaireGame.Entities;
+using System.Collections.Generic;
+
+namespace MillionaireGame.Frontend.Models
+{
+    public class GameViewModel
+    {
+        public int QuestionIndex { get; set; }
+
+        public Question Question { get; set; }
+
+        public IEnumerable<GameStep> GameSteps { get; set; }
+    }
+}
