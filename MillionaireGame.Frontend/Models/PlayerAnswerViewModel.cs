71bf3e95f0b4d2cc371df581095c12f19d58ae70
bohdanD <doroshbohdan1@gmail.com>
2017-06-12 15:47:47 +0300

    add playeranswerviewmodel

---
+++ MillionaireGame.Frontend/Models/PlayerAnswerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MillionaireGame.Frontend.Models
+{
+    public class PlayerAnswerViewModel
+    {
+        public string PlayerAnser { get; set; }
+
+        public int QuestionIndex { get; set; }
+    }
+}
