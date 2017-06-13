58cf1108ebbb1dc73020e9f643e331e4b9daaf67
Oleg Dutsyak <oleg.dystak@gmail.com>
2017-06-13 11:34:42 +0300

    Add custom error pages ;)

---
+++ MillionaireGame.Frontend/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace MillionaireGame.Frontend.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+
+        public ActionResult BadRequest()
+        {
+            Response.StatusCode = 400;
+            return View();
+        }
+
+        public ActionResult InternalError()
+        {
+            Response.StatusCode = 500;
+            return View();
+        }
+    }
+}
