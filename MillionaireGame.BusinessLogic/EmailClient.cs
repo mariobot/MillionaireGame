bd555c19c3fceebeb794899d6753f129ac566e8a
Oleg Dutsyak <oleg.dystak@gmail.com>
2017-06-17 02:03:47 +0300

    SMTP email client for sending messages

---
+++ MillionaireGame.BusinessLogic/EmailClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace MillionaireGame.BusinessLogic
+{
+    public class EmailClient
+    {
+        private readonly EmailConfigurationElement _emailSettings = EmailConfigurationSection.Instance.Email;
+        private readonly SmtpClient _smtp;
+        private readonly MailAddress _fromAddress;
+
+        public EmailClient()
+        {
+            _fromAddress = new MailAddress(_emailSettings.MailFromAddress, _emailSettings.MailFromName);
+            var credential = new NetworkCredential(_fromAddress.Address, _emailSettings.Password);
+
+            _smtp = new SmtpClient
+            {
+                Host = _emailSettings.Host,
+                Port = _emailSettings.Port,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Timeout = 30000,
+                UseDefaultCredentials = false,
+                Credentials = credential
+            };
+        }
+
+        public void Send(string recipientEmail, string title, string text)
+        {
+            var toAddress = new MailAddress(recipientEmail);
+
+            using (var message = new MailMessage(_fromAddress, toAddress) { Subject = title, Body = text })
+            {
+                try
+                {
+                    _smtp.Send(message);
+                }
+                catch (Exception)
+                {
+                    // TODO: some exception handling
+                }
+            }
+        }
+    }
+}
