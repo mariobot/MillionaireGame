c53820ce0d96fd85b42a004af44c596bb44d81a2
Oleg Dutsyak <oleg.dystak@gmail.com>
2017-06-17 02:03:25 +0300

    Implement custon email settings section in Web.config

---
+++ MillionaireGame.BusinessLogic/EmailConfigurationElement.cs
@@ -0,0 +1,43 @@
+﻿using System.Configuration;
+
+namespace MillionaireGame.BusinessLogic
+{
+    public class EmailConfigurationElement : ConfigurationElement
+    {
+        [ConfigurationProperty(nameof(MailFromAddress), IsRequired = true)]
+        public string MailFromAddress
+        {
+            get => (string)base[nameof(MailFromAddress)];
+            set => base[nameof(MailFromAddress)] = value;
+        }
+
+        [ConfigurationProperty(nameof(MailFromName))]
+        public string MailFromName
+        {
+            get => (string)base[nameof(MailFromName)];
+            set => base[nameof(MailFromName)] = value;
+        }
+
+        [ConfigurationProperty(nameof(Host), IsRequired = true)]
+        public string Host
+        {
+            get => (string)base[nameof(Host)];
+            set => base[nameof(Host)] = value;
+        }
+
+        [ConfigurationProperty(nameof(Port), IsRequired = true)]
+        [IntegerValidator(MinValue = 0, MaxValue = 65535)]
+        public int Port
+        {
+            get => (int)base[nameof(Port)];
+            set => base[nameof(Port)] = value;
+        }
+
+        [ConfigurationProperty(nameof(Password), IsRequired = true)]
+        public string Password
+        {
+            get => (string)base[nameof(Password)];
+            set => base[nameof(Password)] = value;
+        }
+    }
+}
---
+++ MillionaireGame.BusinessLogic/EmailConfigurationSection.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace MillionaireGame.BusinessLogic
+{
+    public sealed class EmailConfigurationSection : ConfigurationSection
+    {
+        private static EmailConfigurationSection _instance;
+
+        [ConfigurationProperty(nameof(Email), IsRequired = true)]
+        public EmailConfigurationElement Email
+        {
+            get => (EmailConfigurationElement)base[nameof(Email)];
+            set => base[nameof(Email)] = value;
+        }
+
+        public static EmailConfigurationSection Instance => _instance ?? (_instance =
+                                                                (EmailConfigurationSection)ConfigurationManager.GetSection(
+                                                                    nameof(EmailConfigurationSection)));
+
+        private EmailConfigurationSection() { }
+    }
+}
