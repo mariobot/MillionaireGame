67f36a842bcf2709be5d0de9558cc59deeeeb5b1
Oleg Dutsyak <oleg.dystak@gmail.com>
2017-06-17 02:04:00 +0300

    Implement SHA1 hashing

---
+++ MillionaireGame.BusinessLogic/SHA1HashingService.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+// ReSharper disable InconsistentNaming
+
+namespace MillionaireGame.BusinessLogic
+{
+    public class SHA1HashingService : IHashingService
+    {
+        public string GetPasswordHash(string password)
+        {
+            using (var hasher = new SHA1Managed())
+            {
+                var hash = hasher.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var resultHash = new StringBuilder(hash.Length * 2);
+
+                foreach (var b in hash)
+                {
+                    resultHash.Append(b.ToString("X2"));
+                }
+
+                return resultHash.ToString();
+            }
+        }
+    }
+}
