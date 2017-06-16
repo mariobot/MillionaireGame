adb3a2fda2d8af714ede128f4f12d9774f2ce599
bohdanD <doroshbohdan1@gmail.com>
2017-06-16 16:58:06 +0300

    Add GameHint

---
+++ MillionaireGame.BusinessLogic/Abstract/IGameHint.cs
@@ -0,0 +1,21 @@
+﻿using MillionaireGame.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MillionaireGame.BusinessLogic.Abstraction
+{
+    public interface IGameHint
+    {
+        /// <summary>
+        /// Implements fifty on fifty hint.
+        /// </summary>
+        /// <param name="question">Initial question</param>
+        /// <returns>Question that contains only two titled answers, other should be empty string</returns>
+        Question FiftyPercentsHint(Question question);
+
+        //TODO: Add friend call hint
+    }
+}
---
+++ MillionaireGame.BusinessLogic/Concrete/GameHint.cs
@@ -0,0 +1,32 @@
+﻿using MillionaireGame.BusinessLogic.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MillionaireGame.Entities;
+
+namespace MillionaireGame.BusinessLogic.Concrete
+{
+    public class GameHint : IGameHint
+    {
+        public Question FiftyPercentsHint(Question question)
+        {
+            Random rnd = new Random();
+            int outIndex = rnd.Next(0, 3);
+            for (int i = 0; i < 2; i++)
+            {
+                var answer = question.Answers.ElementAt(outIndex);
+                if (answer.Correct || string.IsNullOrEmpty(answer.Title))
+                {
+                    i--;
+                }
+                else
+                {
+                    answer.Title = "";
+                }
+            }
+            return question;
+        }
+    }
+}
