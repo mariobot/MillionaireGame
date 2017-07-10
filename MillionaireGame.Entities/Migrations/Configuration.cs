f5907a73a10444b59256edb65cecef24badb68d4
bohdanD <doroshbohdan1@gmail.com>
2017-07-10 15:18:36 +0300

    Add database intializer.

--- MillionaireGame.Entities/Migrations/Configuration.cs
+++ MillionaireGame.Entities/Migrations/Configuration.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.Data.Entity.Migrations;
-using System.IO;
-using Newtonsoft.Json;
 
 namespace MillionaireGame.Entities.Migrations
 {
@@ -27,31 +23,7 @@ namespace MillionaireGame.Entities.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
-            string text;                   
-            using (var reader = new StreamReader(Path.Combine(AppDomain.CurrentDomain.GetData("DataDirectory").ToString(), "Questions.json")))
-            {
-                text = reader.ReadToEnd();
-            }
-            var questions = JsonConvert.DeserializeObject<List<Question>>(text);
-
-            foreach (var item in questions)
-            {
-                var answerStatistics = new List<AnswerStatistic>();
-                
-                var questionStatistic = new QuestionStatistic
-                {
-                    QuestionCounter = 0,
-                    AnswerStatistics = answerStatistics
-                };
-                for (int i = 0; i < 4; i++)
-                {
-                    questionStatistic.AnswerStatistics.Add(new AnswerStatistic { AnswerCounter = 0 });
-                }
-                
-                item.QuestionStatistic = questionStatistic;
-                context.Questions.AddOrUpdate(item);
-            }
-            context.SaveChanges();
+            
         }
     }
 }
---
+++ MillionaireGame.Entities/MillionaireGameDbInitializer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using MillionaireGame.Entities.Migrations;
+using Newtonsoft.Json;
+
+namespace MillionaireGame.Entities
+{
+    public class MillionaireGameDbInitializer : CreateDatabaseIfNotExists<LoggerContext>
+    {
+        protected override void Seed(LoggerContext context)
+        {
+            base.Seed(context);
+
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<LoggerContext, Configuration>());
+
+            string text;
+            using (var reader = new StreamReader(Path.Combine(AppDomain.CurrentDomain.GetData("DataDirectory").ToString(), "Questions.json")))
+            {
+                text = reader.ReadToEnd();
+            }
+            var questions = JsonConvert.DeserializeObject<List<Question>>(text);
+
+            foreach (var item in questions)
+            {
+                var answerStatistics = new List<AnswerStatistic>();
+
+                var questionStatistic = new QuestionStatistic
+                {
+                    QuestionCounter = 0,
+                    AnswerStatistics = answerStatistics
+                };
+                for (int i = 0; i < 4; i++)
+                {
+                    questionStatistic.AnswerStatistics.Add(new AnswerStatistic { AnswerCounter = 0 });
+                }
+
+                item.QuestionStatistic = questionStatistic;
+                context.Questions.Add(item);
+            }
+            context.SaveChanges();
+        }
+    }
+}
